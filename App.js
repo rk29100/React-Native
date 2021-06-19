1acf35328bf5f1fbb80bbe1ef05d8b1fb5ee1ab4
Rutuja Kulkarni <rsk29100@gmail.com>
2021-06-19 21:55:37 +0530

    DoneCommited

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Main from './app/component/Main';
+
+
+export default class App extends React.Component {
+  render(){
+    return(
+      <Main/>
+      
+    );
+  }
+}
+
